ffc558dae28d77f16247d59c3dae7a8d3e56b519
Tomaz Canabrava <tcanabrava@kde.org>
2020-12-29 11:41:17 +0000

    Teste inicial do palafita.

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+extern crate glfw;
+extern crate gl;
+// include the OpenGL type aliases
+use gl::types::*;
+
+use glfw::{Action, Context, Key};
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3,3));
+
+    let (mut window, events) 
+        = glfw.create_window(
+            300,
+            300,
+            "Hello this is window",
+            glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    window.set_title("Hello this is window");
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+    window.make_current();
+
+
+    let(width, height) = window.get_framebuffer_size();
+    unsafe { gl::Viewport(0, 0, width, height) };
+
+    while !window.should_close() {
+        window.swap_buffers();
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_event(&mut window, event);
+        }
+    }
+}
+
+trait WindowEventHandler {
+    fn key_event(self: &mut Self, key: Key, scancode: i32, action: Action, modifiers: glfw::Modifiers);
+    fn resize_event(self: &mut Self, width: i32, height: i32);
+}
+
+impl WindowEventHandler for glfw::Window{
+    fn key_event(self: &mut Self, key: Key, _scancode: i32, _action: Action, _modifiers: glfw::Modifiers) {
+        match key {
+            Key::Escape => { self.set_should_close(true); }
+            _ => { println!("Outra tecla tocada"); }
+        }
+    }
+    fn resize_event(self: &mut Self, width: i32, height: i32) {
+        unsafe { gl::Viewport(0, 0, width, height) };
+    }
+}
+
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    // println!("Window event received {:?}", event);
+    match event {
+        glfw::WindowEvent::Key(key, scancode, action, modifiers) => {
+            window.key_event(key, scancode, action, modifiers)
+        }
+        glfw::WindowEvent::Close => {
+            println!("Close requested!");
+        }
+        glfw::WindowEvent::Refresh => {
+            println!("Refresh requested");
+        }
+        glfw::WindowEvent::FramebufferSize(width, height) => {
+            // println!("Resized to {0}, {1}", width, height);
+            unsafe { gl::Viewport(0, 0, width, height) };
+        }
+        _ => {}
+    }
+}
