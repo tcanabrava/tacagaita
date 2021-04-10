6ba8860413e8317491aaac1458f567f1c192da66
Patrick José Pereira <patrickelectric@gmail.com>
2021-04-10 08:54:33 -0300

    main: Use ranges to create cube_indexes
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -110,15 +110,7 @@ fn main() -> Result<(), anyhow::Error> {
         -0.5,  0.5, -0.5, 0.0, 0.0, 0.0, 0.0, 1.0
     ];
 
-    #[rustfmt::skip]
-    let cube_indexes: Vec<i32> = vec![
-         0,  1,  2,  3,  4,  5,
-         6,  7,  8,  9, 10, 11,
-        12, 13, 14, 15, 16, 17,
-        18, 19, 20, 21, 22, 23,
-        24, 25, 26, 27, 28, 29,
-        30, 31, 32, 33, 34, 35,
-    ];
+    let cube_indexes: Vec<i32> = (0..=35).collect();
 
     let mut cube_1 = Geometry::from_data(
         &cube,
